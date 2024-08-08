0e593f99026d5d9ea7b3316d4e5c6ff5798f9720
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-08-08 17:56:18 -0400

    Setting up server rendering

--- src/components/ui/ChatInput.tsx
+++ src/components/ui/ChatInput.tsx
@@ -3,9 +3,12 @@ import { Input } from "@/components/ui/input"
 import { PaperPlaneIcon } from "@radix-ui/react-icons"
 
 export function ChatInput() {
+  // Grab the messaghe from some sort of state.
+  // This is a server component, so we'll need to fetch the message from the server.
+
   return (
     <div className="flex w-full items-center space-x-2">
-      <Input type="text" placeholder="Ask me anything" />
+      <Input type="text" placeholder="Ask me anything"  />
       <Button type="submit">
        <PaperPlaneIcon style={{transform: 'rotate(-45git deg)' }}/>
       </Button>
