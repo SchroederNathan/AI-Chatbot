2d0d99ccaf84be8e8390392766e1e324b0b0a9db
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-08-14 12:19:32 -0400

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-      </head>
+      <head></head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
@@ -28,6 +28,8 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+
+        <Analytics />
       </body>
     </html>
   );
